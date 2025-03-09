6b801a0076b2bc912afe122b8f236c61a61d940a
Le Vivilet <levivlet@pm.me>
2025-03-09 19:36:03 +0100

    feature: add functions to create iframeInspector virtual dom

---
+++ packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
@@ -0,0 +1,17 @@
+import type { Message } from '../Message/Message.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetMessageVirtualDom from '../GetMessageVirtualDom/GetMessageVirtualDom.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getIframeInspectorVirtualDom = (messages: readonly Message[]): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.IFrameInspector),
+      childCount: messages.length,
+    },
+    ...messages.flatMap(GetMessageVirtualDom.getMessageVirtualDom),
+  ]
+}
