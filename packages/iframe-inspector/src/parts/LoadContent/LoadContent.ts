71e75446e2663e724f8f1c8400c9acf4bc63d115
Le Vivilet <levivlet@pm.me>
2025-03-10 21:45:00 +0100

    feature: improve loadContent function

--- packages/iframe-inspector/src/parts/LoadContent/LoadContent.ts
+++ packages/iframe-inspector/src/parts/LoadContent/LoadContent.ts
@@ -1,3 +1,17 @@
+import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
+import * as IframeInspectorViewStates from '../IframeInspectorViewStates/IframeInspectorViewStates.ts'
+
 export const loadContent = (uid: number): void => {
-  // TODO
+  const { newState } = IframeInspectorViewStates.get(uid)
+  const updated: IframeInspectorState = {
+    ...newState,
+    messages: [
+      {
+        method: 'test',
+        params: [],
+        id: 1,
+      },
+    ],
+  }
+  IframeInspectorViewStates.set(uid, newState, updated)
 }
