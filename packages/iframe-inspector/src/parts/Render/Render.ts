bad04faebf4ce0619e99b62287e00555a83e513c
Le Vivilet <levivlet@pm.me>
2025-03-10 19:32:51 +0100

    feature: use uid for rendering

--- packages/iframe-inspector/src/parts/Render/Render.ts
+++ packages/iframe-inspector/src/parts/Render/Render.ts
@@ -1,8 +1,9 @@
-import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
 import * as Diff from '../Diff/Diff.ts'
+import * as IframeInspectorViewStates from '../IframeInspectorViewStates/IframeInspectorViewStates.ts'
 
-export const doRender = (oldState: IframeInspectorState, newState: IframeInspectorState): readonly any[] => {
+export const doRender = (uid: number): readonly any[] => {
+  const { oldState, newState } = IframeInspectorViewStates.get(uid)
   const diffResult = Diff.diff(oldState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
   return commands
