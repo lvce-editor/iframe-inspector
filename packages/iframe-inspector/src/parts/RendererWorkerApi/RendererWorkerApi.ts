05f87cfa2940618b8b70354eb9869bd2b6168026
Le Vivilet <levivlet@pm.me>
2025-03-10 22:04:13 +0100

    lint

--- packages/iframe-inspector/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++ packages/iframe-inspector/src/parts/RendererWorkerApi/RendererWorkerApi.ts
@@ -1,4 +1,5 @@
 export interface RendererWorkerApi {
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
   readonly 'IframeWorker.registerInterceptor': (id: number, port: MessagePort) => Promise<void>
   readonly 'IframeWorker.unregisterInterceptor': (id: number) => Promise<void>
 }
