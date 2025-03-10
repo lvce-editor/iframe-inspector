25be6a825fe1ccf640029072885582de7dd4ba07
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-10 21:55:22 +0100

    feature: improve message rendering (#14)

--- packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
@@ -1,7 +1,16 @@
 import type { Message } from '../Message/Message.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getMessageVirtualDom = (message: Message): readonly VirtualDomNode[] => {
-  return [text(message.method)]
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.IframeInspectorMessage,
+      childCount: 1,
+    },
+    text(message.method),
+  ]
 }
