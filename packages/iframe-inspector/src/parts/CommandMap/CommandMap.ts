fa5c23f7132a13cc93206a74ebff06ad71a40fae
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-10 19:28:43 +0100

    feature: add loadContent command (#11)

--- packages/iframe-inspector/src/parts/CommandMap/CommandMap.ts
+++ packages/iframe-inspector/src/parts/CommandMap/CommandMap.ts
@@ -1,11 +1,13 @@
 import * as Create from '../Create/Create.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
+import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Render from '../Render/Render.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
 
 export const commandMap = {
   'IframeInspector.getCommandIds': GetCommandIds.getCommandIds,
   'IframeInspector.create': Create.create,
+  'IframeInspector.loadContent': LoadContent.loadContent,
   'IframeInspector.renderEventListeners': RenderEventListeners.renderEventListeners,
   'IframeInspector.render': Render.doRender,
 }
