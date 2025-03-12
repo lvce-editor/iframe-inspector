407908c81b71d1c1317d6ed3bad98bd49295a5b9
Le Vivilet <levivlet@pm.me>
2025-03-12 20:47:14 +0100

    feature: handle webview message

--- packages/iframe-inspector/src/parts/Interceptor/Interceptor.ts
+++ packages/iframe-inspector/src/parts/Interceptor/Interceptor.ts
@@ -1,9 +1,17 @@
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
+// TODO remove event listener on dispose
+const handleMessage = (event: any): void => {
+  // TODO
+  // console.log('got event', event.data)
+}
+
 export const register = async (id: number): Promise<void> => {
-  const { port1 } = new MessageChannel()
+  const { port1, port2 } = new MessageChannel()
   await ParentRpc.invokeAndTransfer('WebView.registerInterceptor', id, port1)
   // TODO create rpc with port2
+  port2.addEventListener('message', handleMessage)
+  port2.start()
 }
 
 export const unregister = async (id: number): Promise<void> => {
