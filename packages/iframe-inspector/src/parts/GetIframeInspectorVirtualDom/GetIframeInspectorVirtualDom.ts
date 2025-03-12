1c0b5db2d9b2b983e5b24593a9537b0836dcb661
Le Vivilet <levivlet@pm.me>
2025-03-12 22:40:50 +0100

    feature: add iframe inspector header

--- packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
@@ -4,14 +4,32 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetMessagesVirtualDom from '../GetMessagesVirtualDom/GetMessagesVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getIframeInspectorVirtualDom = (messages: readonly Message[]): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.IFrameInspector),
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'IframeInspectorHeader',
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'IframeInspectorHeaderData',
       childCount: 1,
     },
+    text('Data'),
+    {
+      type: VirtualDomElements.Div,
+      className: 'IframeInspectorHeaderLength',
+      childCount: 1,
+    },
+    text('Length'),
     ...GetMessagesVirtualDom.getMessagesVirtualDom(messages),
   ]
 }
