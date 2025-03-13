1ac6d0c72df787ee62645f7a387eb0a37aa47eaa
Le Vivilet <levivlet@pm.me>
2025-03-13 21:46:33 +0100

    feature: add selectIndex test

---
+++ packages/iframe-inspector/test/SelectIndex.test.ts
@@ -0,0 +1,27 @@
+import { beforeEach, expect, test } from '@jest/globals'
+import type { IframeInspectorState } from '../src/parts/IframeInspectorState/IframeInspectorState.ts'
+import * as IframeInspectorViewStates from '../src/parts/IframeInspectorViewStates/IframeInspectorViewStates.ts'
+import * as SelectIndex from '../src/parts/SelectIndex/SelectIndex.ts'
+
+beforeEach(() => {
+  const state: IframeInspectorState = {
+    messages: [],
+    uid: 1,
+    messageVersion: 0,
+    selectedIndex: -1,
+  }
+  IframeInspectorViewStates.set(1, state, state)
+})
+
+test('selectIndex', () => {
+  SelectIndex.selectIndex(1, 2)
+  const { newState } = IframeInspectorViewStates.get(1)
+  expect(newState.selectedIndex).toBe(2)
+})
+
+test('selectIndex - multiple updates', () => {
+  SelectIndex.selectIndex(1, 2)
+  SelectIndex.selectIndex(1, 3)
+  const { newState } = IframeInspectorViewStates.get(1)
+  expect(newState.selectedIndex).toBe(3)
+})
