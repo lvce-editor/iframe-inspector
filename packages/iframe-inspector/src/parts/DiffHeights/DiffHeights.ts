71a502dbdb15be317d632bd8242047ac27294688
Le Vivilet <levivlet@pm.me>
2025-03-14 20:51:15 +0100

    feature: add diffHeights function

---
+++ packages/iframe-inspector/src/parts/DiffHeights/DiffHeights.ts
@@ -0,0 +1,8 @@
+import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
+
+export const diffType = DiffType.RenderHeights
+
+export const isEqual = (oldState: IframeInspectorState, newState: IframeInspectorState): boolean => {
+  return oldState.messagesHeight === newState.messagesHeight
+}
--- packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
+++ packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
@@ -1,3 +1,4 @@
 import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffHeights from '../DiffHeights/DiffHeights.ts'
 
-export const modules = [DiffItems]
+export const modules = [DiffItems, DiffHeights]
