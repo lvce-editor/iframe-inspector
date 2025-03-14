7138b894f1df3dd2f7e766be039db53686e065fa
Le Vivilet <levivlet@pm.me>
2025-03-14 19:37:45 +0100

    lint

--- packages/iframe-inspector/src/parts/HandleClick/HandleClick.ts
+++ packages/iframe-inspector/src/parts/HandleClick/HandleClick.ts
@@ -1,11 +1,13 @@
 import * as IframeInspectorViewStates from '../IframeInspectorViewStates/IframeInspectorViewStates.ts'
 
 export const handleClick = (uid: number, eventX: number, eventY: number): void => {
-  console.log({ uid, eventX, eventY })
-
   const { newState } = IframeInspectorViewStates.get(uid)
   const { headerHeight } = newState
   const actualY = eventY - headerHeight
-  console.log({ actualY })
+
+  // @ts-ignore abcdef
+
+  globalThis.act = actualY
+
   // TODO
 }
