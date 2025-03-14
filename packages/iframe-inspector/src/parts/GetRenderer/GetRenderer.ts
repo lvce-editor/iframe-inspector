e6809a1435eaa0b4eded0912c4fdaab2840ac06a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-14 20:59:30 +0100

    feature: add heights renderer (#74)

--- packages/iframe-inspector/src/parts/GetRenderer/GetRenderer.ts
+++ packages/iframe-inspector/src/parts/GetRenderer/GetRenderer.ts
@@ -1,11 +1,14 @@
 import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import * as RenderHeights from '../RenderHeights/RenderHeights.ts'
 import * as RenderMessages from '../RenderMessages/RenderMessages.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
     case DiffType.RenderItems:
       return RenderMessages.renderMessage
+    case DiffType.RenderHeights:
+      return RenderHeights.renderHeights
     default:
       throw new Error('unknown renderer')
   }
