050999d32c7d316754dc210e933a6d07ac148770
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-14 18:19:09 +0100

    feature: render selected item (#49)

--- packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
@@ -3,17 +3,19 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetHeaderVirtualDom from '../GetHeaderVirtualDom/GetHeaderVirtualDom.ts'
 import * as GetMessagesVirtualDom from '../GetMessagesVirtualDom/GetMessagesVirtualDom.ts'
+import * as GetSelectedContentVirtualDom from '../GetSelectedContentVirtualDom/GetSelectedContentVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getIframeInspectorVirtualDom = (messages: readonly Message[]): readonly VirtualDomNode[] => {
+export const getIframeInspectorVirtualDom = (messages: readonly Message[], selectedIndex: number): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.IFrameInspector),
-      childCount: 2,
+      childCount: 3,
     },
     ...GetHeaderVirtualDom.getHeaderVirtualDom(),
     ...GetMessagesVirtualDom.getMessagesVirtualDom(messages),
+    ...GetSelectedContentVirtualDom.getSelectedContentVirtualDom(messages, selectedIndex),
   ]
 }
--- packages/iframe-inspector/src/parts/RenderMessages/RenderMessages.ts
+++ packages/iframe-inspector/src/parts/RenderMessages/RenderMessages.ts
@@ -4,6 +4,6 @@ import * as MessageState from '../MessageState/MessageState.ts'
 
 export const renderMessage = (oldState: IframeInspectorState, newState: IframeInspectorState): readonly any[] => {
   const messages = MessageState.getMessages()
-  const dom = GetIframeInspectorVirtualDom.getIframeInspectorVirtualDom(messages)
+  const dom = GetIframeInspectorVirtualDom.getIframeInspectorVirtualDom(messages, newState.selectedIndex)
   return ['Viewlet.setDom2', dom]
 }
