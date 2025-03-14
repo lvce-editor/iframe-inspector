dba37355d6706ab234c6e9e881f825d07c6df774
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-14 20:30:37 +0100

    feature: register keyboard navigation commands (#63)

--- packages/iframe-inspector/src/parts/CommandMap/CommandMap.ts
+++ packages/iframe-inspector/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,10 @@
 import * as Create from '../Create/Create.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
+import * as HandleArrowDown from '../HandleArrowDown/HandleArrowDown.ts'
+import * as HandleArrowUp from '../HandleArrowUp/HandleArrowUp.ts'
 import * as HandleClick from '../HandleClick/HandleClick.ts'
+import * as HandleEnd from '../HandleEnd/HandleEnd.ts'
+import * as HandleHome from '../HandleHome/HandleHome.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Render from '../Render/Render.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
@@ -11,6 +15,10 @@ export const commandMap = {
   'IframeInspector.create': Create.create,
   'IframeInspector.loadContent': LoadContent.loadContent,
   'IframeInspector.handleClick': HandleClick.handleClick,
+  'IframeInspector.handleArrowDown': HandleArrowDown.handleArrowDown,
+  'IframeInspector.handleArrowUp': HandleArrowUp.handleArrowUp,
+  'IframeInspector.handleHome': HandleHome.handleHome,
+  'IframeInspector.handleEnd': HandleEnd.handleEnd,
   'IframeInspector.renderEventListeners': RenderEventListeners.renderEventListeners,
   'IframeInspector.render': Render.doRender,
   'IframeInspector.selectIndex': SelectIndex.selectIndex,
