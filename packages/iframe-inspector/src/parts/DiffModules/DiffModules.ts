29fdd795e7e82971cd8570d3cd610ab833cbe812
Le Vivilet <levivlet@pm.me>
2025-03-14 20:52:39 +0100

    lint

--- packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
+++ packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
@@ -1,4 +1,4 @@
-import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffHeights from '../DiffHeights/DiffHeights.ts'
+import * as DiffItems from '../DiffItems/DiffItems.ts'
 
 export const modules = [DiffItems, DiffHeights]
