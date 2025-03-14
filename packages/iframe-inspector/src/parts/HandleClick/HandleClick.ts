39295d54c4f3b1e48bd564bc0881ce860eb10b4a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-14 19:56:55 +0100

    feature: call selectIndex on click (#54)

--- packages/iframe-inspector/src/parts/HandleClick/HandleClick.ts
+++ packages/iframe-inspector/src/parts/HandleClick/HandleClick.ts
@@ -1,13 +1,10 @@
+import * as GetIndex from '../GetIndex/GetIndex.ts'
 import * as IframeInspectorViewStates from '../IframeInspectorViewStates/IframeInspectorViewStates.ts'
+import * as SelectIndex from '../SelectIndex/SelectIndex.ts'
 
 export const handleClick = (uid: number, eventX: number, eventY: number): void => {
   const { newState } = IframeInspectorViewStates.get(uid)
-  const { headerHeight } = newState
-  const actualY = eventY - headerHeight
-
-  // @ts-ignore abcdef
-
-  globalThis.act = actualY
-
-  // TODO
+  const { headerHeight, y, itemHeight } = newState
+  const index = GetIndex.getIndex(headerHeight, y, eventY, itemHeight)
+  SelectIndex.selectIndex(uid, index)
 }
