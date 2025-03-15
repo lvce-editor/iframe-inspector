2fd036a55d3bcc24db672d94fbf8f6ac13b4c780
Le Vivilet <levivlet@pm.me>
2025-03-15 15:52:42 +0100

    feature: use table dom for message row

--- packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
@@ -1,30 +1,26 @@
 import type { MessageViewModel } from '../MessageViewModel/MessageViewModel.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as GetMessageClassName from '../GetMessageClassName/GetMessageClassName.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-
-const getClassName = (isSelected: boolean): string => {
-  return MergeClassNames.mergeClassNames(ClassNames.IframeInspectorMessage, isSelected ? ClassNames.IframeInspectorMessageSelected : '')
-}
 
 export const getMessageVirtualDom = (message: MessageViewModel): readonly VirtualDomNode[] => {
   const { isSelected, messagePreview, messagePreviewLength } = message
   return [
     {
-      type: VirtualDomElements.Div,
-      className: getClassName(isSelected),
+      type: VirtualDomElements.Tr,
+      className: GetMessageClassName.getMessageClassName(isSelected),
       childCount: 2,
     },
     {
-      type: VirtualDomElements.Pre,
+      type: VirtualDomElements.Td,
       className: ClassNames.InspectorMessageData,
       childCount: 1,
     },
     text(messagePreview),
     {
-      type: VirtualDomElements.Div,
+      type: VirtualDomElements.Td,
       className: ClassNames.InspectorMessageLength,
       childCount: 1,
     },
