0fc8453f8ebbc60c4102a10bde2af512f1447226
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-16 00:32:14 +0100

    feature: add more complex webview fixture (#124)

---
+++ packages/e2e/fixtures/complex-webview/worker.js
@@ -0,0 +1,17 @@
+const create = async ({ port }) => {
+  const { count } = await port.invoke('getCount')
+  const newCount = count.value + 1
+  await port.invoke('setCount', { count: { value: newCount } })
+}
+
+const commands = {
+  'WebView.create': create,
+}
+
+const main = async () => {
+  const uri = new URL('../../node_modules/@lvce-editor/extension-host-sub-worker/dist/extensionHostSubWorkerMainApi.js', import.meta.url).toString()
+  const { listen, commandMap } = await import(uri)
+  await listen({ ...commandMap, ...commands })
+}
+
+main()
