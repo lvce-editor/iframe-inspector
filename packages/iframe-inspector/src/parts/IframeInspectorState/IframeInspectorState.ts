72be993b845ea914d45e83f4c1d2751ccd6346e8
Le Vivilet <levivlet@pm.me>
2025-03-16 00:19:47 +0100

    type

--- packages/iframe-inspector/src/parts/IframeInspectorState/IframeInspectorState.ts
+++ packages/iframe-inspector/src/parts/IframeInspectorState/IframeInspectorState.ts
@@ -15,5 +15,5 @@ export interface IframeInspectorState {
   readonly isResizing: boolean
   readonly resizeStartY: number
   readonly resizeStartHeight: number
-  readonly expandedPaths?: readonly string[]
+  readonly expandedPaths: readonly string[]
 }
