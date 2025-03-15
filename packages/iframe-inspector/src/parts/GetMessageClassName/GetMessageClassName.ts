fb89b1b975c40309756ff1cabf8fc9f6ef4e0d32
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-15 16:34:45 +0100

    feature: add isEven property to displayMessages (#102)
    * feature: add isEven property to displayMessages
    
    * update
    
    * add test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test

--- packages/iframe-inspector/src/parts/GetMessageClassName/GetMessageClassName.ts
+++ packages/iframe-inspector/src/parts/GetMessageClassName/GetMessageClassName.ts
@@ -1,6 +1,10 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 
-export const getMessageClassName = (isSelected: boolean): string => {
-  return MergeClassNames.mergeClassNames(ClassNames.TableRow, isSelected ? ClassNames.TableRowSelected : '')
+export const getMessageClassName = (isSelected: boolean, isEven: boolean): string => {
+  return MergeClassNames.mergeClassNames(
+    ClassNames.TableRow,
+    isSelected ? ClassNames.TableRowSelected : '',
+    isEven ? ClassNames.TableRowEven : ClassNames.TableRowOdd,
+  )
 }
