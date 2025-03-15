a50b98629e54a5a431bb4d0f16f0708242f057de
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-15 23:36:45 +0100

    feature: move selected message string into separate file (#110)

--- packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
@@ -3,21 +3,13 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetResizerVirtualDom from '../GetResizerVirtualDom/GetResizerVirtualDom.ts'
 import * as GetSelectedContentVirtualDom from '../GetSelectedContentVirtualDom/GetSelectedContentVirtualDom.ts'
+import * as GetSelectedMessageString from '../GetSelectedMessageString/GetSelectedMessageString.ts'
 import * as GetTableWrapperVirtualDom from '../GetTableWrapperVirtualDom/GetTableWrapperVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-const getSelectedString = (messages: readonly MessageViewModel[], selectedIndex: number): string => {
-  if (selectedIndex === -1) {
-    return ''
-  }
-  const selectedMessage = messages[selectedIndex]
-  const stringified = JSON.stringify(selectedMessage, null, 2)
-  return stringified
-}
-
 export const getIframeInspectorVirtualDom = (messages: readonly MessageViewModel[], selectedIndex: number): readonly VirtualDomNode[] => {
-  const selectedMessageString = getSelectedString(messages, selectedIndex)
+  const selectedMessageString = GetSelectedMessageString.getSelectedMessageString(messages, selectedIndex)
   return [
     {
       type: VirtualDomElements.Div,
