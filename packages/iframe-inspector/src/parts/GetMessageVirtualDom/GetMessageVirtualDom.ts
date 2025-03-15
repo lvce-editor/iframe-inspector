b1b5242d988b5135382a73c7f6c98c20f06f26d2
Le Vivilet <levivlet@pm.me>
2025-03-15 16:14:55 +0100

    update

--- packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
@@ -6,11 +6,11 @@ import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getMessageVirtualDom = (message: MessageViewModel): readonly VirtualDomNode[] => {
-  const { isSelected, messagePreview, messagePreviewLength } = message
+  const { isSelected, messagePreview, messagePreviewLength, isEven } = message
   return [
     {
       type: VirtualDomElements.Tr,
-      className: GetMessageClassName.getMessageClassName(isSelected),
+      className: GetMessageClassName.getMessageClassName(isSelected, isEven),
       childCount: 2,
     },
     {
