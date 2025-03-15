dfd0311af22080e98dbd661547f8c285e6d62994
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-15 16:48:14 +0100

    feature: make message maxPreviewLength configurable (#108)

--- packages/iframe-inspector/test/GetMessagePreview.test.ts
+++ packages/iframe-inspector/test/GetMessagePreview.test.ts
@@ -2,21 +2,24 @@ import { expect, test } from '@jest/globals'
 import * as GetMessagePreview from '../src/parts/GetMessagePreview/GetMessagePreview.ts'
 
 test('getMessagePreview - short message', () => {
-  const message = { method: 'test', params: [1, 2, 3] }
-  expect(GetMessagePreview.getMessagePreview(message)).toBe('{"method":"test","params":[1,2,3]}')
+  const maxLength = 100
+  const message = { method: 'test', params: [1, 2, 3] } as any
+  expect(GetMessagePreview.getMessagePreview(message, maxLength)).toBe('{"method":"test","params":[1,2,3]}')
 })
 
 test('getMessagePreview - long message', () => {
   const message = {
     method: 'test',
     params: ['very long string '.repeat(20)],
-  }
-  expect(GetMessagePreview.getMessagePreview(message)).toBe(
+  } as any
+  const maxLength = 100
+  expect(GetMessagePreview.getMessagePreview(message, maxLength)).toBe(
     '{"method":"test","params":["very long string very long string very long string very long string very...',
   )
 })
 
 test('getMessagePreview - empty object', () => {
-  const message = {}
-  expect(GetMessagePreview.getMessagePreview(message)).toBe('{}')
+  const maxLength = 100
+  const message = {} as any
+  expect(GetMessagePreview.getMessagePreview(message, maxLength)).toBe('{}')
 })
