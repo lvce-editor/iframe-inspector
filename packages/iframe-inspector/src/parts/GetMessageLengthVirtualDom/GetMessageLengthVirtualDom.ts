3fad5d6b78deff1d262f1a7e08fe9469bf64db2c
Le Vivilet <levivlet@pm.me>
2025-03-16 23:44:54 +0100

    feature: use numeric token type for message length

--- packages/iframe-inspector/src/parts/GetMessageLengthVirtualDom/GetMessageLengthVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetMessageLengthVirtualDom/GetMessageLengthVirtualDom.ts
@@ -1,6 +1,7 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getMessageLengthVirtualDom = (messageLength: string): readonly VirtualDomNode[] => {
@@ -8,6 +9,11 @@ export const getMessageLengthVirtualDom = (messageLength: string): readonly Virt
     {
       type: VirtualDomElements.Td,
       className: ClassNames.TableCell,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Span,
+      className: MergeClassNames.mergeClassNames('Token Numeric'),
       childCount: 1,
     },
     text(messageLength),
