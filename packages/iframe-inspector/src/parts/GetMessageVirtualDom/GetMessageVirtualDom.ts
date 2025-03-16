e7ac56e505a46e5cdeb4e3138e7b2bf65e0836b9
Le Vivilet <levivlet@pm.me>
2025-03-16 23:15:44 +0100

    feature: move message preview into separate file

--- packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetMessageVirtualDom/GetMessageVirtualDom.ts
@@ -1,9 +1,9 @@
 import type { MessageViewModel } from '../MessageViewModel/MessageViewModel.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetMessageClassName from '../GetMessageClassName/GetMessageClassName.ts'
+import * as GetMessageLengthVirtualDom from '../GetMessageLengthVirtualDom/GetMessageLengthVirtualDom.ts'
+import * as GetMessagePreviewVirtualDom from '../GetMessagePreviewVirtualDom/GetMessagePreviewVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getMessageVirtualDom = (message: MessageViewModel): readonly VirtualDomNode[] => {
   const { isSelected, messagePreview, messagePreviewLength, isEven } = message
@@ -13,17 +13,7 @@ export const getMessageVirtualDom = (message: MessageViewModel): readonly Virtua
       className: GetMessageClassName.getMessageClassName(isSelected, isEven),
       childCount: 2,
     },
-    {
-      type: VirtualDomElements.Td,
-      className: ClassNames.TableCell,
-      childCount: 1,
-    },
-    text(messagePreview),
-    {
-      type: VirtualDomElements.Td,
-      className: ClassNames.TableCell,
-      childCount: 1,
-    },
-    text(messagePreviewLength),
+    ...GetMessagePreviewVirtualDom.getMessagePreviewVirtualDom(messagePreview),
+    ...GetMessageLengthVirtualDom.getMessageLengthVirtualDom(messagePreviewLength),
   ]
 }
