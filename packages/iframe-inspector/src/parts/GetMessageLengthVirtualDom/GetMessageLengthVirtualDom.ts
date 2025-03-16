2d31aaa2007ea3d4e8286140ad254ad80a2427c8
Le Vivilet <levivlet@pm.me>
2025-03-16 23:47:01 +0100

    lint

--- packages/iframe-inspector/src/parts/GetMessageLengthVirtualDom/GetMessageLengthVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetMessageLengthVirtualDom/GetMessageLengthVirtualDom.ts
@@ -1,7 +1,7 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getMessageLengthVirtualDom = (messageLength: string): readonly VirtualDomNode[] => {
