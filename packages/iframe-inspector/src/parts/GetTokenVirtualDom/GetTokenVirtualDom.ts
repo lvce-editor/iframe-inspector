71586f73f5f92a4543df8d00e35d943bda775041
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-17 22:47:49 +0100

    feature: use flatmap (#174)

--- packages/iframe-inspector/src/parts/GetTokenVirtualDom/GetTokenVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetTokenVirtualDom/GetTokenVirtualDom.ts
@@ -1,8 +1,10 @@
+import type { Token } from '../Token/Token.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as TokenParentNodes from '../TokenParentNodes/TokenParentNodes.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
-export const getTokenVirtualDom = (tokenType: string, tokenText: string): readonly VirtualDomNode[] => {
+export const getTokenVirtualDom = (token: Token): readonly VirtualDomNode[] => {
+  const { tokenType, tokenText } = token
   // @ts-ignore
   return [TokenParentNodes[tokenType], text(tokenText)]
 }
--- packages/iframe-inspector/src/parts/GetTokensVirtualDom/GetTokensVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetTokensVirtualDom/GetTokensVirtualDom.ts
@@ -3,10 +3,5 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as GetTokenVirtualDom from '../GetTokenVirtualDom/GetTokenVirtualDom.ts'
 
 export const getTokensVirtualDom = (messageTokens: readonly Token[]): readonly VirtualDomNode[] => {
-  const allNodes: VirtualDomNode[] = []
-  for (let i = 0; i < messageTokens.length; i += 1) {
-    const nodes = GetTokenVirtualDom.getTokenVirtualDom(messageTokens[i].tokenType, messageTokens[i].tokenText)
-    allNodes.push(...nodes)
-  }
-  return allNodes
+  return messageTokens.flatMap(GetTokenVirtualDom.getTokenVirtualDom)
 }
