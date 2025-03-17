f2097331cfef0fb3d92ba23e47bf81dec85b4770
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-17 19:40:13 +0100

    feature: simplify mesage viewModel (#150)

--- packages/iframe-inspector/src/parts/CreateMessageViewModel/CreateMessageViewModel.ts
+++ packages/iframe-inspector/src/parts/CreateMessageViewModel/CreateMessageViewModel.ts
@@ -3,19 +3,23 @@ import type { MessageViewModel } from '../MessageViewModel/MessageViewModel.ts'
 import * as GetMessagePreview from '../GetMessagePreview/GetMessagePreview.ts'
 import * as TokenizeJson from '../TokenizeJson/TokenizeJson.ts'
 
+const createSingleMessageViewModel = (message: Message, maxLength: number, index: number, selectedIndex: number): MessageViewModel => {
+  const stringified = JSON.stringify(message)
+  const preview = GetMessagePreview.getMessagePreview(stringified, maxLength)
+  const tokens = TokenizeJson.tokenizeJson(preview)
+  return {
+    isSelected: index === selectedIndex,
+    messagePreview: preview,
+    messageRaw: message,
+    messagePreviewLength: String(preview.length),
+    isEven: index % 2 === 0,
+    messageTokens: tokens,
+  }
+}
+
 export const createMessageViewModel = (messages: readonly Message[], selectedIndex: number): readonly MessageViewModel[] => {
   const maxLength = 100
   return messages.map((message, index) => {
-    const stringified = JSON.stringify(message)
-    const preview = GetMessagePreview.getMessagePreview(stringified, maxLength)
-    const tokens = TokenizeJson.tokenizeJson(preview)
-    return {
-      isSelected: index === selectedIndex,
-      messagePreview: preview,
-      messageRaw: message,
-      messagePreviewLength: String(preview.length),
-      isEven: index % 2 === 0,
-      messageTokens: tokens,
-    }
+    return createSingleMessageViewModel(message, maxLength, index, selectedIndex)
   })
 }
