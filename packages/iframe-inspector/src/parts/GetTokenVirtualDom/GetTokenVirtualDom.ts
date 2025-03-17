4ed818c97ef4d764a0ff7f530407d8238fbc418b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-17 22:24:52 +0100

    feature: optimize tokens virtual dom (#170)

--- packages/iframe-inspector/src/parts/GetTokenVirtualDom/GetTokenVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetTokenVirtualDom/GetTokenVirtualDom.ts
@@ -1,14 +1,8 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as TokenParentNodes from '../TokenParentNodes/TokenParentNodes.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getTokenVirtualDom = (tokenType: string, tokenText: string): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.Span,
-      className: `Token ${tokenType}`,
-      childCount: 1,
-    },
-    text(tokenText),
-  ]
+  // @ts-ignore
+  return [TokenParentNodes[tokenType], text(tokenText)]
 }
