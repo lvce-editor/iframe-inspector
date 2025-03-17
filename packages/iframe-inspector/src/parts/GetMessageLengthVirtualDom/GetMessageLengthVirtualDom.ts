03998beb51dd841ad1d9d77da3949e411c93ed7f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-17 22:04:36 +0100

    feature: optimize messageLength dom (#166)

--- packages/iframe-inspector/src/parts/GetMessageLengthVirtualDom/GetMessageLengthVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetMessageLengthVirtualDom/GetMessageLengthVirtualDom.ts
@@ -4,18 +4,18 @@ import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
+const tableCell: VirtualDomNode = {
+  type: VirtualDomElements.Td,
+  className: ClassNames.TableCell,
+  childCount: 1,
+}
+
+const span: VirtualDomNode = {
+  type: VirtualDomElements.Span,
+  className: MergeClassNames.mergeClassNames('Token Numeric'),
+  childCount: 1,
+}
+
 export const getMessageLengthVirtualDom = (messageLength: string): readonly VirtualDomNode[] => {
-  return [
-    {
-      type: VirtualDomElements.Td,
-      className: ClassNames.TableCell,
-      childCount: 1,
-    },
-    {
-      type: VirtualDomElements.Span,
-      className: MergeClassNames.mergeClassNames('Token Numeric'),
-      childCount: 1,
-    },
-    text(messageLength),
-  ]
+  return [tableCell, span, text(messageLength)]
 }
