95b77c0f49368e1716384f51cc2d818114ffe366
Le Vivilet <levivlet@pm.me>
2025-03-17 21:48:52 +0100

    feature: optimize diff function

--- packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
+++ packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
@@ -1,4 +1,6 @@
 import * as DiffHeights from '../DiffHeights/DiffHeights.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 
-export const modules = [DiffItems, DiffHeights]
+export const modules = [DiffItems.isEqual, DiffHeights.isEqual]
+
+export const numbers = [DiffItems.diffType, DiffHeights.diffType]
