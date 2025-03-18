6eab6aa864b4fa94e2cee5b20ebfd80625c420d7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-18 21:01:05 +0100

    feature: improve handleFocus (#192)

--- packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
+++ packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
@@ -1,4 +1,14 @@
-export const handleFocus = (): void => {
-  // TODO: Implement focus handling
-  return
+import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
+import * as IframeInspectorViewStates from '../IframeInspectorViewStates/IframeInspectorViewStates.ts'
+
+export const handleFocus = (uid: number): void => {
+  const { newState } = IframeInspectorViewStates.get(uid)
+  if (newState.isFocused) {
+    return
+  }
+  const updatedState: IframeInspectorState = {
+    ...newState,
+    isFocused: true,
+  }
+  IframeInspectorViewStates.set(uid, newState, updatedState)
 }
