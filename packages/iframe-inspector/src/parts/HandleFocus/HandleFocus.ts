677f9d2c8e3f8ba6305ff1e2cd032c9351e281c2
Le Vivilet <levivlet@pm.me>
2025-03-18 21:12:02 +0100

    feature: send focus change to renderer worker

--- packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
+++ packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
@@ -1,8 +1,12 @@
 import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
 import * as IframeInspectorViewStates from '../IframeInspectorViewStates/IframeInspectorViewStates.ts'
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
-export const handleFocus = (uid: number): void => {
+export const handleFocus = async (uid: number): Promise<void> => {
   const { newState } = IframeInspectorViewStates.get(uid)
+  // TODO make focus handling more functional
+  await ParentRpc.invoke('Focus.setFocus', WhenExpression.FocusIframeInspector)
   if (newState.isFocused) {
     return
   }
