8ac1ded255b161f798c3de97670aa6f20268cfaa
Le Vivilet <levivlet@pm.me>
2025-03-18 20:06:39 +0100

    feature: move column into separate function

--- packages/iframe-inspector/src/parts/GetColGroupVirtualDom/GetColGroupVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetColGroupVirtualDom/GetColGroupVirtualDom.ts
@@ -1,16 +1,22 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
+const getColVirtualDom = (width: string): VirtualDomNode => {
+  return {
+    type: VirtualDomElements.Col,
+    className: 'Col',
+    width,
+    childCount: 0,
+  }
+}
+
 export const getColGroupVirtualDom = (columnWidths: readonly string[]): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.ColGroup,
+      className: 'ColGroup',
       childCount: columnWidths.length,
     },
-    ...columnWidths.map((width) => ({
-      type: VirtualDomElements.Col,
-      width,
-      childCount: 0,
-    })),
+    ...columnWidths.map(getColVirtualDom),
   ]
 }
