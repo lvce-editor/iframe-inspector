7d6e7731353c36af7d4f688ec4a45ad24381927e
Le Vivilet <levivlet@pm.me>
2025-03-18 20:44:28 +0100

    feature: add focus diff function

--- packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
+++ packages/iframe-inspector/src/parts/DiffModules/DiffModules.ts
@@ -1,6 +1,7 @@
+import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffHeights from '../DiffHeights/DiffHeights.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 
-export const modules = [DiffItems.isEqual, DiffHeights.isEqual]
+export const modules = [DiffItems.isEqual, DiffHeights.isEqual, DiffFocus.isEqual]
 
-export const numbers = [DiffItems.diffType, DiffHeights.diffType]
+export const numbers = [DiffItems.diffType, DiffHeights.diffType, DiffFocus.diffType]
