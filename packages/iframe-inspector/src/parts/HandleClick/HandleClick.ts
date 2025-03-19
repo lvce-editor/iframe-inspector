9e73d4a1cc46f00bd24c736a8953ca45114911db
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-19 20:11:40 +0100

    feature: take into account filterHeight for click events (#208)

--- packages/iframe-inspector/src/parts/HandleClick/HandleClick.ts
+++ packages/iframe-inspector/src/parts/HandleClick/HandleClick.ts
@@ -4,7 +4,7 @@ import * as SelectIndex from '../SelectIndex/SelectIndex.ts'
 
 export const handleClick = (uid: number, eventX: number, eventY: number): void => {
   const { newState } = IframeInspectorViewStates.get(uid)
-  const { headerHeight, y, itemHeight } = newState
-  const index = GetIndex.getIndex(headerHeight, y, eventY, itemHeight)
+  const { headerHeight, y, itemHeight, filterHeight } = newState
+  const index = GetIndex.getIndex(headerHeight, y, eventY, itemHeight, filterHeight)
   SelectIndex.selectIndex(uid, index)
 }
