bde11480fe1a59d2957aee7e3b19a9a6f6759f6e
Le Vivilet <levivlet@pm.me>
2025-03-19 20:40:26 +0100

    improve filter

--- packages/iframe-inspector/src/parts/GetFilteredMessages/GetFilteredMessages.ts
+++ packages/iframe-inspector/src/parts/GetFilteredMessages/GetFilteredMessages.ts
@@ -2,5 +2,8 @@ import type { Message } from '../Message/Message.ts'
 import { matchesFilterText } from '../Message/MatchesFilterText.ts'
 
 export const getFilteredMessages = (messages: readonly Message[], filterText: string): readonly Message[] => {
+  if (!filterText) {
+    return messages
+  }
   return messages.filter((message) => matchesFilterText(message, filterText))
 }
