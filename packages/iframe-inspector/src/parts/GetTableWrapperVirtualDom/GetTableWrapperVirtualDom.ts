faf1950e4e239aed96e3efc7cec40a897077c5ff
Le Vivilet <levivlet@pm.me>
2025-03-19 20:46:48 +0100

    feature: move filter dom into separate file

--- packages/iframe-inspector/src/parts/GetTableWrapperVirtualDom/GetTableWrapperVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetTableWrapperVirtualDom/GetTableWrapperVirtualDom.ts
@@ -1,6 +1,7 @@
 import type { MessageViewModel } from '../MessageViewModel/MessageViewModel.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetFilterVirtualDom from '../GetFilterVirtualDom/GetFilterVirtualDom.ts'
 import * as GetTableVirtualDom from '../GetTableVirtualDom/GetTableVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
@@ -11,28 +12,5 @@ const parentNode: VirtualDomNode = {
 }
 
 export const getTableWrapperVirtualDom = (messages: readonly MessageViewModel[], columnWidths: readonly string[]): readonly VirtualDomNode[] => {
-  return [
-    parentNode,
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.FilterSection,
-      childCount: 1,
-    },
-    {
-      type: VirtualDomElements.Input,
-      className: ClassNames.FilterInput,
-      placeholder: 'Filter messages...',
-      onInput: 'handleFilterInput',
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'IframeInspectorGrid',
-      role: 'application',
-      tabIndex: 0,
-      childCount: 1,
-      onFocusIn: 'handleFocus',
-      onBlur: 'handleBlur',
-    },
-    ...GetTableVirtualDom.getTableVirtualDom(messages, columnWidths),
-  ]
+  return [parentNode, ...GetFilterVirtualDom.getFilterVirtualDom(), ...GetTableVirtualDom.getTableVirtualDom(messages, columnWidths)]
 }
