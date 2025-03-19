3a4d3b84f7dfafdb05bde4b8a8b4e625216e29d7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-19 20:48:59 +0100

    feature: move filter dom into separate file (#215)

---
+++ packages/iframe-inspector/src/parts/GetFilterVirtualDom/GetFilterVirtualDom.ts
@@ -0,0 +1,31 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as InputName from '../InputName/InputName.ts'
+import * as Role from '../Role/Role.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getFilterVirtualDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.FilterSection,
+      childCount: 1,
+    },
+    {
+      type: VirtualDomElements.Input,
+      className: ClassNames.FilterInput,
+      placeholder: 'Filter messages...',
+      onInput: 'handleFilterInput',
+      name: InputName.IframeInspectorFilterInput,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: 'IframeInspectorGrid',
+      role: Role.Application,
+      tabIndex: 0,
+      childCount: 1,
+      onFocusIn: 'handleFocus',
+      onBlur: 'handleBlur',
+    },
+  ]
+}
---
+++ packages/iframe-inspector/src/parts/InputName/InputName.ts
@@ -0,0 +1,1 @@
+export const IframeInspectorFilterInput = 'IframeInspectorFilterInput'
