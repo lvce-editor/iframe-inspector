70a77a891c8ecdd6d740f7578a272d759e624fff
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-19 20:46:14 +0100

    feature: filter messages before rendering (#213)
    * feature: filter messages before rendering
    
    * improve filter
    
    * fix

--- packages/iframe-inspector/src/parts/RenderMessages/RenderMessages.ts
+++ packages/iframe-inspector/src/parts/RenderMessages/RenderMessages.ts
@@ -1,13 +1,15 @@
 import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
 import * as CreateMessageViewModels from '../CreateMessageViewModels/CreateMessageViewModels.ts'
 import * as CreateSelectedMessageViewModel from '../CreateSelectedMessageViewModel/CreateSelectedMessageViewModel.ts'
+import * as GetFilteredMessages from '../GetFilteredMessages/GetFilteredMessages.ts'
 import * as GetIframeInspectorVirtualDom from '../GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts'
 import * as MessageState from '../MessageState/MessageState.ts'
 
 export const renderMessage = (oldState: IframeInspectorState, newState: IframeInspectorState): readonly any[] => {
   const messages = MessageState.getMessages()
   const maxLength = 100
-  const viewModels = CreateMessageViewModels.createMessageViewModels(messages, newState.selectedIndex, maxLength, newState.filterText)
+  const filteredMessages = GetFilteredMessages.getFilteredMessages(messages, newState.filterText)
+  const viewModels = CreateMessageViewModels.createMessageViewModels(filteredMessages, newState.selectedIndex, maxLength, newState.filterText)
   const selectedModel = CreateSelectedMessageViewModel.createSelectedMessageViewModel(messages, newState.selectedIndex, newState.expandedPaths)
   const dom = GetIframeInspectorVirtualDom.getIframeInspectorVirtualDom(viewModels, selectedModel, newState.selectedIndex, newState.columnWidths)
   return ['Viewlet.setDom2', dom]
