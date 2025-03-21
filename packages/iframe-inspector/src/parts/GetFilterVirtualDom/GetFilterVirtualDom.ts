a61384377bb973b0a858faaf0e3a53b4b3748df3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-21 18:41:20 +0100

    feature: use i18n (#228)

--- packages/iframe-inspector/src/parts/GetFilterVirtualDom/GetFilterVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetFilterVirtualDom/GetFilterVirtualDom.ts
@@ -1,10 +1,12 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListeners from '../DomEventListeners/DomEventListeners.ts'
+import * as IframeInspectorStrings from '../IframeInspectorStrings/IframeInspectorStrings.ts'
 import * as InputName from '../InputName/InputName.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 export const getFilterVirtualDom = (): readonly VirtualDomNode[] => {
+  const placeholder = IframeInspectorStrings.filterMessages()
   return [
     {
       type: VirtualDomElements.Div,
@@ -14,7 +16,7 @@ export const getFilterVirtualDom = (): readonly VirtualDomNode[] => {
     {
       type: VirtualDomElements.Input,
       className: ClassNames.FilterInput,
-      placeholder: 'Filter messages...',
+      placeholder,
       onInput: DomEventListeners.HandleFilterInput,
       name: InputName.IframeInspectorFilterInput,
     },
