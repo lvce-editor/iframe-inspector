2aeb08205a0b673ba9a15b35911f862fcdc83079
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-30 10:39:29 +0200

    feature: add jsonrpc property to all messages (#235)
    * feature: add jsonrpc property to all messages
    
    * test
    
    * test
    
    * test
    
    * test
    
    * spacing

--- packages/iframe-inspector/src/parts/HandleMessage/HandleMessage.ts
+++ packages/iframe-inspector/src/parts/HandleMessage/HandleMessage.ts
@@ -5,7 +5,7 @@ import * as MessageState from '../MessageState/MessageState.ts'
 // TODO remove event listener on dispose
 export const handleMessage = (data: any): void => {
   const actual = data.params[0]
-  const message = typeof actual === 'string' ? { method: actual, params: [] } : actual
+  const message = typeof actual === 'string' ? { jsonrpc: '2.0', method: actual, params: [] } : actual
   MessageState.addMessage(message)
   const keys = IframeInspectorViewStates.getKeys()
   for (const key of keys) {
