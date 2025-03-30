da64adc6d51b351ae346af190b3ea70580a5d8f8
Le Vivilet <levivlet@pm.me>
2025-03-30 20:33:14 +0200

    feature: add dispose command

--- packages/iframe-inspector/src/parts/IframeInspectorViewStates/IframeInspectorViewStates.ts
+++ packages/iframe-inspector/src/parts/IframeInspectorViewStates/IframeInspectorViewStates.ts
@@ -1,23 +1,4 @@
 import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
+import * as ViewletRegistry from '../ViewletRegistry/ViewletRegistry.ts'
 
-const states = Object.create(null)
-
-export const get = (
-  uid: string | number,
-): {
-  oldState: IframeInspectorState
-  newState: IframeInspectorState
-} => {
-  return states[uid]
-}
-
-export const getKeys = (): readonly string[] => {
-  return Object.keys(states)
-}
-
-export const set = (uid: string | number, oldState: IframeInspectorState, newState: IframeInspectorState): void => {
-  states[uid] = {
-    oldState,
-    newState,
-  }
-}
+export const { get, set, dispose } = ViewletRegistry.create<IframeInspectorState>()
