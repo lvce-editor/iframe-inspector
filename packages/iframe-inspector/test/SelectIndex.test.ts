3d59a374a5a04765f2ca7eeaa3b60afeaa773c24
Le Vivilet <levivlet@pm.me>
2025-03-30 10:30:20 +0200

    feature: add createDefaultState helper function

--- packages/iframe-inspector/test/SelectIndex.test.ts
+++ packages/iframe-inspector/test/SelectIndex.test.ts
@@ -1,31 +1,10 @@
 import { beforeEach, expect, test } from '@jest/globals'
-import type { IframeInspectorState } from '../src/parts/IframeInspectorState/IframeInspectorState.ts'
+import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as IframeInspectorViewStates from '../src/parts/IframeInspectorViewStates/IframeInspectorViewStates.ts'
 import * as SelectIndex from '../src/parts/SelectIndex/SelectIndex.ts'
 
 beforeEach(() => {
-  const state: IframeInspectorState = {
-    messages: [],
-    uid: 1,
-    messageVersion: 0,
-    selectedIndex: -1,
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    headerHeight: 0,
-    itemHeight: 0,
-    messagesHeight: 0,
-    isResizing: false,
-    resizeStartY: 0,
-    resizeStartHeight: 0,
-    expandedPaths: [],
-    selectedContentItemHeight: 20,
-    columnWidths: [],
-    isFocused: false,
-    filterText: '',
-    filterHeight: 0,
-  }
+  const state = CreateDefaultState.createDefaultState()
   IframeInspectorViewStates.set(1, state, state)
 })
 
