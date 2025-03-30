7a732ba5cb19646cc29e92c891b51641f028b84d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-30 10:19:51 +0200

    feature: hide bottom panel if there are no messages (#233)

--- packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
+++ packages/iframe-inspector/src/parts/GetIframeInspectorVirtualDom/GetIframeInspectorVirtualDom.ts
@@ -3,8 +3,7 @@ import type { SelectedMessageViewModel } from '../SelectedMessageViewModel/Selec
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetFilterVirtualDom from '../GetFilterVirtualDom/GetFilterVirtualDom.ts'
-import * as GetResizerVirtualDom from '../GetResizerVirtualDom/GetResizerVirtualDom.ts'
-import * as GetSelectedContentVirtualDom from '../GetSelectedContentVirtualDom/GetSelectedContentVirtualDom.ts'
+import * as GetIframeInspectorBottomVirtualDom from '../GetIframeInspectorBottomVirtualDom/GetIframeInspectorBottomVirtualDom.ts'
 import * as GetTableWrapperVirtualDom from '../GetTableWrapperVirtualDom/GetTableWrapperVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
@@ -15,15 +14,15 @@ export const getIframeInspectorVirtualDom = (
   selectedIndex: number,
   columnWidths: readonly string[],
 ): readonly VirtualDomNode[] => {
+  const hasMessages = messages.length > 0
   return [
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.IFrameInspector),
-      childCount: 4,
+      childCount: hasMessages ? 4 : 2,
     },
     ...GetFilterVirtualDom.getFilterVirtualDom(),
     ...GetTableWrapperVirtualDom.getTableWrapperVirtualDom(messages, columnWidths),
-    ...GetResizerVirtualDom.getResizerVirtualDom(),
-    ...GetSelectedContentVirtualDom.getSelectedContentVirtualDom(selectedModel, selectedIndex),
+    ...GetIframeInspectorBottomVirtualDom.getIframeInspectorBottomVirtualDom(messages, selectedModel, selectedIndex),
   ]
 }
