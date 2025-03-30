b232cc3bdf0a8995f035b756a52082dda230aebe
Le Vivilet <levivlet@pm.me>
2025-03-30 10:40:44 +0200

    feature: use createDefaultState helper function

--- packages/iframe-inspector/test/DiffHeights.test.ts
+++ packages/iframe-inspector/test/DiffHeights.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@jest/globals'
 import type { IframeInspectorState } from '../src/parts/IframeInspectorState/IframeInspectorState.ts'
+import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as DiffHeights from '../src/parts/DiffHeights/DiffHeights.ts'
 import * as DiffType from '../src/parts/DiffType/DiffType.ts'
 
@@ -8,100 +9,34 @@ test('diffType', () => {
 })
 
 test('isEqual - same height', () => {
+  const defaultState = CreateDefaultState.createDefaultState()
   const oldState: IframeInspectorState = {
+    ...defaultState,
     messageVersion: 1,
-    messages: [],
-    uid: 1,
-    selectedIndex: -1,
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    headerHeight: 0,
-    itemHeight: 0,
     messagesHeight: 100,
-    isResizing: false,
-    resizeStartY: 0,
-    resizeStartHeight: 0,
-    expandedPaths: [],
-    selectedContentItemHeight: 20,
-    columnWidths: [],
-    isFocused: false,
-    filterText: '',
-    filterHeight: 0,
   }
 
   const newState: IframeInspectorState = {
+    ...defaultState,
     messageVersion: 1,
-    messages: [],
-    uid: 1,
-    selectedIndex: -1,
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    headerHeight: 0,
-    itemHeight: 0,
     messagesHeight: 100,
-    isResizing: false,
-    resizeStartY: 0,
-    resizeStartHeight: 0,
-    expandedPaths: [],
-    selectedContentItemHeight: 20,
-    columnWidths: [],
-    isFocused: false,
-    filterText: '',
-    filterHeight: 0,
   }
 
   expect(DiffHeights.isEqual(oldState, newState)).toBe(true)
 })
 
 test('isEqual - different height', () => {
+  const defaultState = CreateDefaultState.createDefaultState()
   const oldState: IframeInspectorState = {
+    ...defaultState,
     messageVersion: 1,
-    messages: [],
-    uid: 1,
-    selectedIndex: -1,
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    headerHeight: 0,
-    itemHeight: 0,
     messagesHeight: 100,
-    isResizing: false,
-    resizeStartY: 0,
-    resizeStartHeight: 0,
-    expandedPaths: [],
-    selectedContentItemHeight: 20,
-    columnWidths: [],
-    isFocused: false,
-    filterText: '',
-    filterHeight: 0,
   }
 
   const newState: IframeInspectorState = {
+    ...defaultState,
     messageVersion: 1,
-    messages: [],
-    uid: 1,
-    selectedIndex: -1,
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0,
-    headerHeight: 0,
-    itemHeight: 0,
     messagesHeight: 200,
-    isResizing: false,
-    resizeStartY: 0,
-    resizeStartHeight: 0,
-    expandedPaths: [],
-    selectedContentItemHeight: 20,
-    columnWidths: [],
-    isFocused: false,
-    filterText: '',
-    filterHeight: 0,
   }
 
   expect(DiffHeights.isEqual(oldState, newState)).toBe(false)
