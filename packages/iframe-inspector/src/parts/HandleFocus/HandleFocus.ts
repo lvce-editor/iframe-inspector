e824db6ee59de40e81aab820d981604d48bc03c1
Le Vivilet <levivlet@pm.me>
2025-04-07 22:42:59 +0200

    focus

--- packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
+++ packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
@@ -1,23 +1,21 @@
-import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
-import * as IframeInspectorViewStates from '../IframeInspectorViewStates/IframeInspectorViewStates.ts'
+import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
-export const handleFocus = async (uid: number): Promise<void> => {
-  const { newState } = IframeInspectorViewStates.get(uid)
+export const handleFocus = async (newState: IframeInspectorState): Promise<IframeInspectorState> => {
   // Don't set focus if it's coming from the input
   if (newState.inputSource === 1) {
-    return
+    return newState
   }
   await ParentRpc.invoke('Focus.setFocus', WhenExpression.FocusIframeInspector)
   if (newState.isFocused) {
-    return
+    return newState
   }
   const updatedState: IframeInspectorState = {
     ...newState,
     isFocused: true,
     focusId: FocusId.Input,
   }
-  IframeInspectorViewStates.set(uid, newState, updatedState)
+  return updatedState
 }
