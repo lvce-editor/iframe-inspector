15b07d46043bd2cc2d816701440d90fdd9af062f
Le Vivilet <levivlet@pm.me>
2025-04-07 22:47:57 +0200

    test

--- packages/iframe-inspector/test/HandleArrowUp.test.ts
+++ packages/iframe-inspector/test/HandleArrowUp.test.ts
@@ -1,29 +1,31 @@
 import { beforeEach, expect, test } from '@jest/globals'
-import type { IframeInspectorState } from '../src/parts/IframeInspectorState/IframeInspectorState.ts'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as HandleArrowUp from '../src/parts/HandleArrowUp/HandleArrowUp.ts'
+import type { IframeInspectorState } from '../src/parts/IframeInspectorState/IframeInspectorState.ts'
 import * as IframeInspectorViewStates from '../src/parts/IframeInspectorViewStates/IframeInspectorViewStates.ts'
 import * as MessageState from '../src/parts/MessageState/MessageState.ts'
 
 beforeEach(() => {
   MessageState.reset()
+})
+
+test('handleArrowUp - from middle', () => {
   const defaultState = CreateDefaultState.createDefaultState()
   const state: IframeInspectorState = {
     ...defaultState,
     selectedIndex: 1,
   }
-  IframeInspectorViewStates.set(1, state, state)
-})
-
-test('handleArrowUp - from middle', () => {
-  HandleArrowUp.handleArrowUp(1)
-  const { newState } = IframeInspectorViewStates.get(1)
+  const newState = HandleArrowUp.handleArrowUp(state)
   expect(newState.selectedIndex).toBe(0)
 })
 
 test('handleArrowUp - at start', () => {
-  HandleArrowUp.handleArrowUp(1)
-  HandleArrowUp.handleArrowUp(1)
-  const { newState } = IframeInspectorViewStates.get(1)
-  expect(newState.selectedIndex).toBe(0)
+  const defaultState = CreateDefaultState.createDefaultState()
+  let state: IframeInspectorState = {
+    ...defaultState,
+    selectedIndex: 1,
+  }
+  state = HandleArrowUp.handleArrowUp(state)
+  state = HandleArrowUp.handleArrowUp(state)
+  expect(state.selectedIndex).toBe(0)
 })
--- packages/iframe-inspector/test/HandleHome.test.ts
+++ packages/iframe-inspector/test/HandleHome.test.ts
@@ -1,20 +1,14 @@
-import { beforeEach, expect, test } from '@jest/globals'
-import type { IframeInspectorState } from '../src/parts/IframeInspectorState/IframeInspectorState.ts'
+import { expect, test } from '@jest/globals'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as HandleHome from '../src/parts/HandleHome/HandleHome.ts'
-import * as IframeInspectorViewStates from '../src/parts/IframeInspectorViewStates/IframeInspectorViewStates.ts'
+import type { IframeInspectorState } from '../src/parts/IframeInspectorState/IframeInspectorState.ts'
 
-beforeEach(() => {
+test('handleHome', () => {
   const defaultState = CreateDefaultState.createDefaultState()
   const state: IframeInspectorState = {
     ...defaultState,
     selectedIndex: 5,
   }
-  IframeInspectorViewStates.set(1, state, state)
-})
-
-test('handleHome', () => {
-  HandleHome.handleHome(1)
-  const { newState } = IframeInspectorViewStates.get(1)
+  const newState = HandleHome.handleHome(state)
   expect(newState.selectedIndex).toBe(0)
 })
