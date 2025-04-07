e3c0f3b5afc963e74d1e68351b23e98e2bf9cd4f
Le Vivilet <levivlet@pm.me>
2025-04-07 22:45:50 +0200

    simplify focus

--- packages/iframe-inspector/src/parts/HandleBlur/HandleBlur.ts
+++ packages/iframe-inspector/src/parts/HandleBlur/HandleBlur.ts
@@ -1,10 +1,5 @@
 import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
 
 export const handleBlur = (newState: IframeInspectorState): IframeInspectorState => {
-  const updatedState: IframeInspectorState = {
-    ...newState,
-    selectedIndex: -1,
-    inputSource: undefined,
-  }
-  return updatedState
+  return newState
 }
--- packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
+++ packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
@@ -1,14 +1,11 @@
 import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
 import * as FocusId from '../FocusId/FocusId.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as SetFocus from '../SetFocus/SetFocus.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const handleFocus = async (newState: IframeInspectorState): Promise<IframeInspectorState> => {
   // Don't set focus if it's coming from the input
-  if (newState.inputSource === 1) {
-    return newState
-  }
-  await ParentRpc.invoke('Focus.setFocus', WhenExpression.FocusIframeInspector)
+  await SetFocus.setFocus(WhenExpression.FocusIframeInspector)
   if (newState.isFocused) {
     return newState
   }
