db6dcf0d527c4d1f337a6441bb2bacae1968a330
Le Vivilet <levivlet@pm.me>
2025-04-07 23:02:29 +0200

    lint

--- packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
+++ packages/iframe-inspector/src/parts/HandleFocus/HandleFocus.ts
@@ -4,13 +4,11 @@ import * as SetFocus from '../SetFocus/SetFocus.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const handleFocus = async (newState: IframeInspectorState, inputName: string): Promise<IframeInspectorState> => {
-  console.log({ inputName })
   // Don't set focus if it's coming from the input
   await SetFocus.setFocus(WhenExpression.FocusIframeInspector)
   if (newState.isFocused && newState.focusId === FocusId.Input) {
     return newState
   }
-  console.log('handle focus')
   const updatedState: IframeInspectorState = {
     ...newState,
     isFocused: true,
