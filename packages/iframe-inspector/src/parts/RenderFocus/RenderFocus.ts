c0494809ca82c00f770c88a0de3b9de1ab123e37
Le Vivilet <levivlet@pm.me>
2025-04-07 22:33:09 +0200

    feature: focus specific element depending on focusid

--- packages/iframe-inspector/src/parts/RenderFocus/RenderFocus.ts
+++ packages/iframe-inspector/src/parts/RenderFocus/RenderFocus.ts
@@ -1,5 +1,18 @@
 import type { IframeInspectorState } from '../IframeInspectorState/IframeInspectorState.ts'
+import * as FocusId from '../FocusId/FocusId.ts'
+import * as InputName from '../InputName/InputName.ts'
 
+const getSelector = (focusId: number): string => {
+  switch (focusId) {
+    case FocusId.List:
+      return '.IframeInspectorGrid'
+    case FocusId.Input:
+      return InputName.IframeInspectorFilterInput
+    default:
+      return ''
+  }
+}
 export const renderFocus = (oldState: IframeInspectorState, newState: IframeInspectorState): readonly any[] => {
-  return ['Viewlet.focusSelector', '.IframeInspectorGrid']
+  const selector = getSelector(newState.focusId)
+  return ['Viewlet.focusSelector', selector]
 }
