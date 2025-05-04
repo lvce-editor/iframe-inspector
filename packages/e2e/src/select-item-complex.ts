557dff80d8472ef7f66a0b41215c7f2832821d10
Le Vivilet <levivlet@pm.me>
2025-05-04 23:36:59 +0200

    feature: reenable e2e tests

--- packages/e2e/src/select-item-complex.ts
+++ packages/e2e/src/select-item-complex.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'select-item-complex'
 
-export const skip = 1
-
 export const test: Test = async ({ Extension, Main, FileSystem, WebView, expect, Command, Locator }) => {
   // arrange
   await Command.execute('Main.closeAllEditors')
