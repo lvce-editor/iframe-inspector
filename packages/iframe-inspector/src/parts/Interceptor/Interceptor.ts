8efe2ceb7ca5cffb235461dfd091e94bb07681fd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-18 20:38:25 +0200

    fix: types (#270)
    * fix: types
    
    * tsc

--- packages/iframe-inspector/src/parts/Interceptor/Interceptor.ts
+++ packages/iframe-inspector/src/parts/Interceptor/Interceptor.ts
@@ -3,7 +3,6 @@ import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 export const register = async (handleMessage: (data: any) => void): Promise<void> => {
   const id = 1
   const { port1, port2 } = new MessageChannel()
-  // @ts-ignore todo
   await ParentRpc.invokeAndTransfer('WebView.registerInterceptor', id, port1)
   // TODO create rpc with port2
   // TODO remove event listener on dispose
@@ -15,6 +14,5 @@ export const register = async (handleMessage: (data: any) => void): Promise<void
 }
 
 export const unregister = async (id: number): Promise<void> => {
-  // @ts-ignore todo
   await ParentRpc.invoke('WebView.unregisterInterceptor', id)
 }
