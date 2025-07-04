1bd89ce57b41846bde8860bb477dc9736cd97369
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-05 00:20:35 +0200

    feature: rename module (#281)

--- packages/iframe-inspector/src/parts/Interceptor/Interceptor.ts
+++ packages/iframe-inspector/src/parts/Interceptor/Interceptor.ts
@@ -1,9 +1,9 @@
-import * as ParentRpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const register = async (handleMessage: (data: any) => void): Promise<void> => {
   const id = 1
   const { port1, port2 } = new MessageChannel()
-  await ParentRpc.invokeAndTransfer('WebView.registerInterceptor', id, port1)
+  await RendererWorker.invokeAndTransfer('WebView.registerInterceptor', id, port1)
   // TODO create rpc with port2
   // TODO remove event listener on dispose
   const wrappedHandleMessage = (event: any): void => {
@@ -14,5 +14,5 @@ export const register = async (handleMessage: (data: any) => void): Promise<void
 }
 
 export const unregister = async (id: number): Promise<void> => {
-  await ParentRpc.invoke('WebView.unregisterInterceptor', id)
+  await RendererWorker.invoke('WebView.unregisterInterceptor', id)
 }
